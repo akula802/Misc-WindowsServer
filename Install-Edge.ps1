8699d418d5f01f867109c97d7fa28e1ea79d38ea
Grid North Tech <akula802@users.noreply.github.com>
2023-08-16 10:54:53 -0600

    Create Install-Edge.ps1

---
+++ Install-Edge.ps1
@@ -0,0 +1,3 @@
+﻿# A one-liner to install Edge (current) on IE-restricted systems
+
+$client = New-Object System.Net.WebClient ; $client.DownloadFile("https://c2rsetup.officeapps.live.com/c2r/downloadEdge.aspx?platform=Default&source=EdgeStablePage&Channel=Stable&language=en&consent=1", 'C:\temp\edge.exe') ; Start-Sleep -Seconds 5 ;  & 'C:\temp\edge.exe'
